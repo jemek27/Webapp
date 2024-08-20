c67cfc1ce8365cf65cb04cd2b5389682f9a0322b
jemek27 <emajure27@gmail.com>
2024-08-20 15:27:54 +0200

    dropbox beautified

--- scripts/dropbox_search.js
+++ scripts/dropbox_search.js
@@ -4,7 +4,6 @@ const datasets = [
     { label: 'Humidity (%)', dataKey: 'humidity', borderColor: 'rgba(75, 192, 192, 1)', backgroundColor: 'rgba(75, 192, 192, 0.2)' },
     { label: 'Soil moisture (%)', dataKey: 'soil_moisture', borderColor: 'rgba(139, 69, 19, 1)', backgroundColor: 'rgba(139, 69, 19, 0.2)' },
     { label: 'Solar Intensity (W/m²)', dataKey: 'solar_intensity', borderColor: 'rgba(255, 204, 0, 1)', backgroundColor: 'rgba(255, 204, 0, 0.2)' },
-    
 ];
 
 function populateDropdown() {
@@ -19,6 +18,11 @@ function populateDropdown() {
     });
 }
 
+document.getElementById('searchInput').addEventListener('focus', function() {
+    const select = document.getElementById('datasetSelect');
+    select.style.display = 'block';
+});
+
 document.getElementById('searchInput').addEventListener('input', function() {
     const searchTerm = this.value.toLowerCase();
     const options = document.querySelectorAll('#datasetSelect option');
@@ -27,17 +31,50 @@ document.getElementById('searchInput').addEventListener('input', function() {
         const label = option.text.toLowerCase();
         option.style.display = label.includes(searchTerm) ? '' : 'none';
     });
+
+    const visibleOptions = Array.from(options).filter(option => option.style.display !== 'none');
+    const select = document.getElementById('datasetSelect');
+    select.size = Math.min(visibleOptions.length, 5);
 });
 
 document.getElementById('datasetSelect').addEventListener('change', function() {
     const selectedIndex = this.value;
     const selectedDataset = datasets[selectedIndex];
 
-    
+    // Wyświetl wykres na podstawie wybranych danych
     fetchData().then(data => {
         createChart(data, selectedDataset.label, [selectedDataset]);
     });
 });
+
+document.addEventListener('click', function(event) {
+    const select = document.getElementById('datasetSelect');
+    const searchInput = document.getElementById('searchInput');
+
+    if (!searchInput.contains(event.target) && !select.contains(event.target)) {
+        select.style.display = 'none';
+    }
+});
+
+// document.getElementById('searchInput').addEventListener('input', function() {
+//     const searchTerm = this.value.toLowerCase();
+//     const options = document.querySelectorAll('#datasetSelect option');
+
+//     options.forEach(option => {
+//         const label = option.text.toLowerCase();
+//         option.style.display = label.includes(searchTerm) ? '' : 'none';
+//     });
+// });
+
+// document.getElementById('datasetSelect').addEventListener('change', function() {
+//     const selectedIndex = this.value;
+//     const selectedDataset = datasets[selectedIndex];
+
+    
+//     fetchData().then(data => {
+//         createChart(data, selectedDataset.label, [selectedDataset]);
+//     });
+// });
 
 
 populateDropdown();
