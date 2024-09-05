ce67915f4e75b0cdc18c9f59833d3718ca27e93e
jemek27 <emajure27@gmail.com>
2024-09-05 22:24:27 +0200

    connection data in .env

--- backend/my-backend/server.js
+++ backend/my-backend/server.js
@@ -7,12 +7,12 @@ const app = express();
 
 const { Pool } = require('pg');
 const pool = new Pool({
-    user: 'admin',
-    host: 'localhost',
-    database: 'loraproject',
-    password: 'admin',
-    port: 5432,
-});
+    user:       process.env.DB_USER,
+    host:       process.env.DB_HOST,
+    database:   process.env.DB_DATABASE,
+    password:   process.env.DB_PASSWORD,
+    port:       process.env.DB_PORT,
+  });
 app.use(cors());
 
 
